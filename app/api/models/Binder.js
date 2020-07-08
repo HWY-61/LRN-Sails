e3a9a9f477f007e43dd7daf5d3aa13e03e0fd1fc
neisha1618 <neisha1618@gmail.com>
2020-07-07 21:08:16 -0500

    added table name to model

--- app/api/models/Binder.js
+++ app/api/models/Binder.js
@@ -1,6 +1,7 @@
 module.exports = {
+  tableName: "Binder",
   attributes: {
-    // id: { type: 'number', autoIncrement: true },
+    id: { type: 'number', autoIncrement: true },
     user_id: { type: 'number', required: true }, // // need to link foreign key
     event_document_id: { type: 'number', required: true }, // need to link foreign key
   },
